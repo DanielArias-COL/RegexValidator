7d180cfe6f7c07da73af45b22a163986e213a787
ATOMOSX <juand.lopezm@uqvirtual.edu.co>
2024-10-30 08:20:51 -0500

    Se dibuja el automata correctamente, pendiente a cambios

--- dibujar_automata/test.py
+++ dibujar_automata/test.py
@@ -10,11 +10,9 @@ class TestAutomata:
         print(f"Probando expresión regular: '{expr}'")
         automata = self.generador.procesar_expresion(expr)
 
-        # Generar y visualizar el autómata en una imagen
         output_path = f'{nombre_prueba}.png'
         output_path = self.generador.visualizar_automata(output_path=output_path)
 
-        # Confirmar que el archivo se ha creado antes de seguir
         if os.path.exists(output_path):
             print(f"Imagen generada: '{output_path}'")
         else:
@@ -23,14 +21,14 @@ class TestAutomata:
 
     def ejecutar_pruebas(self):
         # Pruebas con diferentes expresiones regulares
-        self.prueba_expresion('a', 'prueba1')  # Expresión simple
-        self.prueba_expresion('ab', 'prueba2')  # Concatenación simple
-        self.prueba_expresion('a|b', 'prueba3')  # Unión de símbolos
-        self.prueba_expresion('(a|b)c', 'prueba4')  # Unión con concatenación
-        self.prueba_expresion('a*', 'prueba5')  # Estrella de Kleene
-        self.prueba_expresion('(ab)*', 'prueba6')  # Concatenación con estrella de Kleene
-        self.prueba_expresion('a+b', 'prueba7')  # Estrella de Kleene con símbolo positivo
-        self.prueba_expresion('(a|b)*c', 'prueba8')  # Unión con estrella de Kleene y concatenación
+        self.prueba_expresion('a', 'prueba1')
+        self.prueba_expresion('ab', 'prueba2')
+        self.prueba_expresion('a|b', 'prueba3')
+        self.prueba_expresion('(a|b)c', 'prueba4')
+        self.prueba_expresion('a*', 'prueba5')
+        self.prueba_expresion('(ab)*', 'prueba6')
+        self.prueba_expresion('a+b', 'prueba7')
+        self.prueba_expresion('(a|b)*c', 'prueba8')
 
 
 if __name__ == "__main__":
